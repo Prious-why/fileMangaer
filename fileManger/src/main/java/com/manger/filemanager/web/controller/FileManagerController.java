3312369522e6d3bd32a1eaef2868fd2eaf703a2b
Prious-why <55051528+Prious-why@users.noreply.github.com>
2022-12-11 12:28:34 +0800

    Add files via upload

---
+++ fileManger/src/main/java/com/manger/filemanager/web/controller/FileManagerController.java
@@ -0,0 +1,16 @@
+package com.manger.filemanager.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author why
+ * @date2022-12-11
+ * 文件管理
+ */
+@RestController
+@RequestMapping("/file")
+public class FileManagerController {
+
+
+}
---
+++ fileManger/src/test/java/com/manger/filemanager/FileMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manger.filemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileMangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
